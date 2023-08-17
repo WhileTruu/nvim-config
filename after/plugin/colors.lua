3dbb513499f0162f201847d8bbb1ef694d4b685d
Marten Truu <marten.truu@gmail.com>
2023-08-17 07:05:48 +0300

    change colors

--- after/plugin/colors.lua
+++ after/plugin/colors.lua
@@ -1,10 +1,22 @@
 function ColorMyPencils(color)
-	color = color or "rose-pine"
-	vim.cmd.colorscheme(color)
+    color = color or "kanagawa"
+    vim.cmd.colorscheme(color)
 
-	vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
-	vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
-
+    vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
+    vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
 end
+
+require("kanagawa").setup({
+    colors = {
+        theme = {
+            all = {
+                ui = {
+                    bg_gutter = "none"
+                }
+            }
+        }
+    },
+})
+
 
 ColorMyPencils()
